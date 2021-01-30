ab05efafe658a087f0941cda7c128f6aafdee7bc
VasuMathukiya0303 <vmathukiya183@rku.ac.in>
2021-01-30 16:36:50 +0530

    Add al CRUD Operation

--- Model/book.js
+++ Model/book.js
@@ -1,8 +1,29 @@
 const mongoose = require('mongoose');
 
 const schema = mongoose.Schema({
-    name:String,
-    qty:Number
-})
+    name:{
+        type:String,
+        required:true,
+        minlength:3
+    },
+    qty:{
+        type:Number,
+        required:true,
+        minlength:1
+    },
+    authors:{
+        type:Array
+    },
+    years:{
+        birthyear:{
+            type:Number
+        },
+        deathyear:{
+            type:Number
+        }
+    }
+    // name:String,
+    // qty:Number
+},{timestamps:true});
 
 module.exports = mongoose.model("Book",schema);
--- route.js
+++ route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const book = require('./Model/book');
 const Book = require('./Model/book');
 const router = express.Router();
 
@@ -6,5 +7,47 @@ router.get("/books",async (req,res)=>{
 
     const book = await Book.find();
     res.send(book);
+});
+
+router.post("/books",async (req,res)=>{
+
+    try {
+        const book = new Book({
+            name:req.body.name,
+            qty:req.body.qty,
+            authors:req.body.authors,
+            years:req.body.years
+        });
+        await book.save();
+        res.send(book);
+    } catch (error) {
+       res.send(book); 
+    }
+    
+});
+router.patch("/books/:id",async(req,res)=>{
+    try{
+        const book = await Book.findOne({_id:req.params.id});
+        book.name = req.body.name;
+
+        // Book.findOneAndUpdate({filter},{update})
+        await book.save();
+        res.send(book);
+
+    } catch(error){
+        res.send(error);
+    }
+});
+
+
+
+router.delete("/books/:id",async(req,res)=>{
+    try{
+    await Book.deleteOne({_id:req.params.id});
+    res.send("deleted"); 
+    }
+    catch{
+        res.send(error);
+    }
 });
 module.exports = router;
